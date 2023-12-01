279da262b7158d3cff5e3436ee1553f5f9a55d44
leen60 <laura.zilinskaite@outlook.com>
2023-12-01 14:05:51 +0200

    environment init

---
+++ src/main/java/com/example/entrance_task_chat_app/EntranceTaskChatAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.entrance_task_chat_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntranceTaskChatAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EntranceTaskChatAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/entrance_task_chat_app/EntranceTaskChatAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.entrance_task_chat_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntranceTaskChatAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
